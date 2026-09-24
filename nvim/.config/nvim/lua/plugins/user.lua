59f94a077d4fa482f85ddfd7f37ce5e32fec02f4
Mexaas <ruslancernaso2@gmail.com>
2026-09-24 22:41:17 +0500

    refactor: user.lua has become shorter

--- nvim/.config/nvim/lua/plugins/user.lua
+++ nvim/.config/nvim/lua/plugins/user.lua
@@ -10,14 +10,14 @@ return {
         theme = "minecraft",
       },
       text = {
-        editing = function(opts) return "Работает с " .. opts.filename end,
-        viewing = function(opts) return "Наблюдает за " .. opts.filename end,
-        workspace = function(opts) return "В " .. opts.workspace end,
-        dashboard = "Не у компа",
-        file_browser = function(opts) return "Ищет что-то в " .. opts.name end
+        editing = function(opts) return "Working with " .. opts.filename end,
+        viewing = function(opts) return "Is watching " .. opts.filename end,
+        workspace = function(opts) return "In " .. opts.workspace end,
+        dashboard = "AFK",
+        file_browser = function(opts) return "Finding something in " .. opts.name end
       },
       editor = {
-        tooltip = "Редактор кода для !skill_issue"
+        tooltip = "not skill issue"
       }
     })
     end
@@ -33,6 +33,7 @@ return {
     end,
   },
 
+  -- Home logo
   {
     "folke/snacks.nvim",
     opts = {
@@ -77,39 +78,13 @@ return {
       },
     },
   },
-
-  { "max397574/better-escape.nvim", enabled = false },
-
-  {
-    "L3MON4D3/LuaSnip",
-    config = function(plugin, opts)
-      local luasnip = require "luasnip"
-      luasnip.filetype_extend("javascript", { "javascriptreact" })
-
-      require "astronvim.plugins.configs.luasnip"(plugin, opts)
-    end,
-  },
-
+  -- () {} "" [] Auto close
   {
     "windwp/nvim-autopairs",
     config = function(plugin, opts)
       require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts)
-      local npairs = require "nvim-autopairs"
-      local Rule = require "nvim-autopairs.rule"
-      local cond = require "nvim-autopairs.conds"
-      npairs.add_rules(
-        {
-          Rule("$", "$", { "tex", "latex" })
-            :with_pair(cond.not_after_regex "%%")
-            :with_pair(
-              cond.not_before_regex("xxx", 3)
-            )
-            :with_move(cond.none())
-            :with_del(cond.not_after_regex "xx")
-            :with_cr(cond.none()),
-        },
-        Rule("a", "a", "-vim")
-      )
     end,
   },
+  { "max397574/better-escape.nvim", enabled = false },
+  { "L3MON4D3/LuaSnip", enabled = false },
 }
